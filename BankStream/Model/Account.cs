943a89f0a715d90b4d907031db7f85f084760c90
Nicolas <margat.nicolas@gmail.com>
2018-10-16 21:21:52 +0200

    ajout des classes user et account

---
+++ BankStream/Model/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankStream.Model
+{
+    class Account
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+
+        public Account()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
---
+++ BankStream/Model/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankStream.Model
+{
+    class User
+    {
+        #region Fields
+
+        public String Name { get; set; }
+        public IList<Account> AccountsList { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public User()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
